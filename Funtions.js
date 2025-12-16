9c47f6b73793bd91909d725daed46552253f0adb
Devendra Solanki <devendrasolanki1485@gmail.com>
2025-12-16 16:25:19 +0530

    object commit

--- Funtions.js
+++ Funtions.js
@@ -116,16 +116,130 @@
 
 
 // give perfact number in given array
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-function perfactnum(a){
-    for(var i = 1; i < a/2; i++){
-        if(i * (i + 1) == a){
-            return a;
-        }
-    }
+// var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+// function perfactnum(a){
+//     for(var i = 1; i < a/2; i++){
+//         if(i * (i + 1) == a){
+//             return a;
+//         }
+//     }
     
 
       
-}
-var result = perfactnum(arr);
-console.log("The perfact numbers are: " + result);
+// }
+// var result = perfactnum(arr);
+// console.log("The perfact numbers are: " + result);
+
+
+
+
+//nexted funtion
+// function outer(){
+//     var name = "dev"
+//     console.log("this outer funtion")
+//     function inner(){
+//         console.log("this inner funtion")
+//         var age =30;
+//         console.log("name is "+name)
+//         console.log("age is "+age)
+//     }
+//    // console.log("age is "+age) // error 
+//     inner()
+
+// }
+// outer();
+
+
+
+
+//cloeser funtion
+
+
+// function outer(){
+//     var name = "dev";
+//     console.log("this outer");
+//     function inner(){
+//         var age = 21;
+//         console.log("Name is"+name);
+//         console.log("age is "+age)
+
+//     }
+//     return inner;
+    
+
+// }
+// let x = outer();
+// x();
+
+//example
+// function Calculat(){
+//     var count =0;
+//     function inc(){
+//         count++;
+//         console.log(count);
+//     }
+//     return inc;
+
+// }
+// let y = Calculat();
+// y();
+// y();
+// y();
+// y();
+
+// function bankAccount(initialBalance) {
+//     let balance = initialBalance; // private variable
+
+//     return function withdraw(amount) {
+//         if (amount <= balance) {
+//             balance -= amount;
+//             console.log("Remaining balance:", balance);
+//         } else {
+//             console.log("Insufficient balance");
+//         }
+//     };
+// }
+
+// const myAccount = bankAccount(15000);
+// myAccount(1000); // 4000
+// myAccount(2000); // 2000                                                                                 
+
+//currying
+
+    // function add(a){
+    //     function sum(b){
+    //         return a+b;
+    //     }
+    //     return sum;
+    // }
+    // let s = add(10);
+    // console.log(s(20))
+
+
+    // ex 
+    //   function add(a){
+    //     function sum(b){
+    //       function total(c){
+    //         return a+b+c;
+    //       }
+    //       return total;
+    //     }
+    //     return sum;
+    // }
+    // let s = add(10);
+    // let cloeser=s(20);
+    // console.log( cloeser(20))
+
+
+
+// function volume(len){
+//     return function(width){
+//         return function(hight){
+//             return hight * width * len;
+//         };
+//     };
+// }
+// const a = volume(2);
+// const b = a(2);
+// console.log(b(2));
+
---
+++ Object.js
@@ -0,0 +1,138 @@
+// var student = {
+//     id:101,
+//     name: "dev",
+//     salary : 3000
+// }
+// console.log(student)
+// console.log(student['name']);
+// console.log(student.salary);
+
+
+
+// var student = {
+
+// }
+// // var employee = new Object();
+
+
+// student['firstname'] = "dev"
+// student.lastname = 'padukon'
+// console.log(student)
+// console.log(student.firstname);
+// console.log(student.lastname)
+
+
+// var student = {
+//     id : 1,
+//     name : 'dev',
+//     salary :554
+
+
+// }
+// for(x in student){
+//     console.log(student[x])
+// }
+
+// var student=[
+//     {
+//         id : 1,
+//     name : 'dev',
+//      salary :554
+//     },
+//        {
+//         id : 2,
+//     name : 'devraj',
+//      salary :55455
+//     },
+//        {
+//         id : 3,
+//     name : 'ravi',
+//      salary :54454
+//     }
+
+// ]
+// // // console.log(student)
+// // console.log(student[0])
+
+// for (var students of student) 
+//     {
+//      console.log(students)
+// }
+
+
+
+
+
+// aray inside the object
+// var student =
+//     {
+//         id : 1,
+//     name : 'dev',
+//      salary :554,
+//      marks : [1,2,34]
+//     }
+//      console.log(student)
+// var student = {
+//      id : 1,
+//     name : 'dev',
+//      salary :554,
+//      display: function(){
+//         console.log("this is display")
+//         console.log(this.id + " and "+this.name)
+//      }
+
+// }
+// student.display()
+
+
+
+// function show (){
+//     console.log("this is show funtion")
+//        console.log("this is display")
+//         console.log(this.id + " and "+this.name)
+
+// }
+
+// var student = {
+//      id : 1,
+//     name : 'dev',
+//      salary :554,
+//      display:show
+// }
+// student.display()
+
+
+
+// var student = {
+//      id : 1,
+//     name : 'dev',
+//      salary :554,
+//      show (){
+//     console.log("this is show funtion")
+//        console.log("this is display")
+//         console.log(this.id + " and "+this.name)
+
+// }
+// }
+// student.show()
+
+
+// var student = {
+//      id : 1,
+//     name : 'dev',
+//      salary :554,
+//     display : () =>{
+//     console.log("this is show funtion")
+//        console.log("this is display")
+//         console.log(student.id + " and "+student.name)
+
+// }
+// }
+// student.display();
+
+
+
+
+
+
+
