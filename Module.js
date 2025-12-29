634d8fd3c4684a213b22b18d86cc876202c3d83f
Devendra Solanki <devendrasolanki1485@gmail.com>
2025-12-29 17:02:30 +0530

    feat: add product feedback form with validation and module integration

---
+++ Module.js
@@ -0,0 +1,13 @@
+alert("welcome in module");
+
+// export let var1 = 35;
+// export function display(){
+//     console.log(var1)
+//     console.log("this is function ")
+// }
+
+
+export default function display(){
+   
+    console.log("this is function ")
+}
---
+++ main.js
@@ -0,0 +1,7 @@
+// import { var1,display } from "./Module.js";
+// console.log(var1);
+// display()
+
+
+import display from "./Module.js";
+display()
