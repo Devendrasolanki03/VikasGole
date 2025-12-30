fedcfac2008ac172fc07a9db4d371717b2b8e1d8
Devendra Solanki <devendrasolanki1485@gmail.com>
2025-12-30 10:26:15 +0530

    refactor: clean up HTML structure and improve form layout in feedback form

---
+++ A-Synchronous.js
@@ -0,0 +1,13 @@
+// console.log("frist");
+// setTimeout(()=>{
+//     console.log("Second");
+// },2000);
+
+// console.log("thrid");
+
+// var a =10;
+// let b = 0;
+// setTimeout(()=>{
+//     b = 20;
+// },2000);
+// console.log("sum is ",(a+b));
